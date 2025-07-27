bfe60a18f6d55e1831c567411df0c1ed14f5144c
Alina Richardson <alina.richardson@selu.edu>
2025-07-27 15:42:43 -0500

    push

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,21 +1,50 @@
 package chapter7;
 
+import java.util.Scanner;
+
 public class BinarySearch {
+
   /** Use binary search to find the key in the list */
   public static int binarySearch(int[] list, int key) {
     int low = 0;
     int high = list.length - 1;
 
     while (high >= low) {
-      int mid = (low + high) / 2;
-      if (key < list[mid])
+      int mid = low + ((high - low) >>> 1); // overflow-safe
+      if (key < list[mid]) {
         high = mid - 1;
-      else if (key == list[mid])
-        return mid;
-      else
+      } else if (key > list[mid]) {
         low = mid + 1;
+      } else {
+        return mid; // found
+      }
     }
 
-    return -low - 1; // Now high < low
+    return -low - 1; // not found: negative insertion point - 1
+  }
+
+  /** Simple driver to try it out */
+  public static void main(String[] args) {
+    try (Scanner in = new Scanner(System.in)) {
+      System.out.print("How many numbers? ");
+      int n = in.nextInt();
+
+      int[] list = new int[n];
+      System.out.println("Enter " + n + " integers in **ascending order**:");
+      for (int i = 0; i < n; i++) {
+        list[i] = in.nextInt();
+      }
+
+      System.out.print("Enter key to search: ");
+      int key = in.nextInt();
+
+      int idx = binarySearch(list, key);
+      if (idx >= 0) {
+        System.out.println("Found " + key + " at index " + idx);
+      } else {
+        int insertionPoint = -idx - 1;
+        System.out.println("Not found. Insertion point = " + insertionPoint);
+      }
+    }
   }
 }
--- LinearSearch.java
+++ LinearSearch.java
@@ -1,12 +1,48 @@
 package chapter7;
 
+import java.util.Scanner;
+
 public class LinearSearch {
+
   /** The method for finding a key in the list */
   public static int linearSearch(int[] list, int key) {
     for (int i = 0; i < list.length; i++) {
       if (key == list[i])
-        return i;
+        return i;  // Key found at index i
     }
-    return -1;
+    return -1;     // Key not found
+  }
+
+  /** Main method to test the linear search */
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    // Ask for the number of elements
+    System.out.print("Enter the number of elements: ");
+    int n = input.nextInt();
+
+    int[] list = new int[n];
+
+    // Input array elements
+    System.out.println("Enter " + n + " integers:");
+    for (int i = 0; i < n; i++) {
+      list[i] = input.nextInt();
+    }
+
+    // Input key to search
+    System.out.print("Enter the key to search for: ");
+    int key = input.nextInt();
+
+    // Perform linear search
+    int index = linearSearch(list, key);
+
+    // Output results
+    if (index != -1) {
+      System.out.println("Key " + key + " found at index " + index + ".");
+    } else {
+      System.out.println("Key " + key + " not found in the list.");
+    }
+
+    input.close();
   }
 }
